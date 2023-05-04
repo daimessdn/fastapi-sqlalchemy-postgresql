c9db4dfd5a41859868d5997eda3f375c041eff4b
daimessdn@gmail.com <daimessdn@gmail.com>
2023-05-04 13:26:49 +0700

    added Item BaseModel

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+# creating the base model
+
+
+class Item(BaseModel):  # serializer (?)
+    id: int
+    name: str
+    description: str
+    price: int
+    on_stock: bool
+
 
 app = FastAPI()
+
 
 @app.get("/")
 def index():
     return {"msg": "hello, world!"}
 
+
 @app.get("/greet/{name}")
 def greet(name: str):
     return {"status": "success", "message": "hello, %s!" % (name)}
+
+
+@app.put("/item/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {
+        "id": item_id,
+        "name": item.name,
+        "description": item.description,
+        "price": item.price,
+        "on_stock": item.on_stock
+    }
